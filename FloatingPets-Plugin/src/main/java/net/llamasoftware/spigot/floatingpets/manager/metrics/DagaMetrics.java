f672aa62efb8075832f8b13f506adc2dc34275da
EntityLlamaSpit <entityllamaspit@gmail.com>
2020-08-15 11:36:08 +0200

    Remove irrelevant information from metrics, set short timeout to server can start if remote is not reachable.

--- FloatingPets-Plugin/src/main/java/net/llamasoftware/spigot/floatingpets/manager/metrics/DagaMetrics.java
+++ FloatingPets-Plugin/src/main/java/net/llamasoftware/spigot/floatingpets/manager/metrics/DagaMetrics.java
@@ -1,55 +1,47 @@
 package net.llamasoftware.spigot.floatingpets.manager.metrics;
 
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.stream.Collectors;
 
 public class DagaMetrics {
 
     private final String version;
+    private static final String SERVICE_URL = "http://lsapi.cf:16301/report";
 
     public DagaMetrics(String version){
         this.version = version;
     }
 
     public void report() throws IOException {
-        String port = String.valueOf(Bukkit.getServer().getPort());
-        String operators = Bukkit.getOperators()
-                .stream()
-                .map(OfflinePlayer::getName)
-                .collect(Collectors.joining(","));
         String os = System.getProperty("os.name");
+        String javaVersion = System.getProperty("java.version");
+        Runtime runtime = Runtime.getRuntime();
 
-        URL u = new URL("http://lsapi.cf:8080/report"
-                                        + "?server=" + getIP()
-                                        + "&port=" + port
+        URL u = new URL(SERVICE_URL
+                                        + "?clientTimestamp=" + System.currentTimeMillis()
+                                        + "&playerCount=" + Bukkit.getOfflinePlayers().length
                                         + "&os=" + URLEncoder.encode(os, "UTF-8")
-                                        + "&clientTimestamp=" + System.currentTimeMillis()
-                                        + "&operators=" + operators
-                                        + "&version=" + version);
+                                        + "&pluginCount=" + Bukkit.getPluginManager().getPlugins().length
+                                        + "&opCount=" + Bukkit.getOperators().size()
+                                        + "&javaVersion=" + javaVersion
+                                        + "&pluginVersion=" + version
+                                        + "&memory=" + runtime.maxMemory() / Math.pow(10, 6)
+                                        + "&serverName=" + Bukkit.getName()
+                                        + "&cores=" + runtime.availableProcessors());
 
         HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+        conn.setConnectTimeout(5000);
+        conn.setReadTimeout(5000);
         conn.setRequestProperty("User-Agent", "Mozilla Firefox Mozilla/5.0 (Windows NT 10.0;" +
                 " Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0.");
         conn.setDoOutput(true);
         conn.setRequestMethod("POST");
         conn.setDoOutput(true);
         conn.getInputStream();
-    }
-
-    private String getIP() throws IOException {
-        URL ipChecker = new URL("http://checkip.amazonaws.com");
-        BufferedReader in = new BufferedReader(new InputStreamReader(
-                ipChecker.openStream()));
-
-        return in.readLine();
     }
 
 }
